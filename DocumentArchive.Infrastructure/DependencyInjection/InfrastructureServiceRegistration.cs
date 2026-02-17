02a088e24ac7b0613c60747adf35e096f62863d7
Vika <vikishul@yandex.ru>
2026-02-17 09:43:47 +0300

    fix: регитсрация репозиториев

--- DocumentArchive.Infrastructure/DependencyInjection/InfrastructureServiceRegistration.cs
+++ DocumentArchive.Infrastructure/DependencyInjection/InfrastructureServiceRegistration.cs
@@ -1,23 +1,42 @@
 ﻿using DocumentArchive.Core.Interfaces;
 using DocumentArchive.Infrastructure.Configuration;
 using DocumentArchive.Infrastructure.Repositories;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace DocumentArchive.Infrastructure.DependencyInjection;
-
 
 public static class InfrastructureServiceRegistration
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        // Регистрируем Options
         services.AddOptions<StorageOptions>()
             .Bind(configuration.GetSection(StorageOptions.SectionName));
-        services.AddScoped<IDocumentRepository, DocumentRepository>();
-        services.AddScoped<IUserRepository, UserRepository>();
-        services.AddScoped<ICategoryRepository, CategoryRepository>();
-        services.AddScoped<IArchiveLogRepository, ArchiveLogRepository>();
+
+        services.AddScoped<IDocumentRepository>(sp =>
+        {
+            var options = sp.GetRequiredService<IOptions<StorageOptions>>();
+            return new DocumentRepository(options);
+        });
+
+        services.AddScoped<IUserRepository>(sp =>
+        {
+            var options = sp.GetRequiredService<IOptions<StorageOptions>>();
+            return new UserRepository(options);
+        });
+
+        services.AddScoped<ICategoryRepository>(sp =>
+        {
+            var options = sp.GetRequiredService<IOptions<StorageOptions>>();
+            return new CategoryRepository(options);
+        });
+
+        services.AddScoped<IArchiveLogRepository>(sp =>
+        {
+            var options = sp.GetRequiredService<IOptions<StorageOptions>>();
+            return new ArchiveLogRepository(options);
+        });
 
         return services;
     }
