0ea8588ee6ee5d08f919e92ee9ebc596dc3ccc97
Vika <vikishul@yandex.ru>
2026-02-20 00:00:55 +0300

    миграции

---
+++ DocumentArchive.Infrastructure/Migrations/20260219121727_AddUpdatedAtToRolePermissionDocumentVersion.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocumentArchive.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUpdatedAtToRolePermissionDocumentVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Roles",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Permissions",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "DocumentVersions",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "UpdatedAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"),
+                column: "UpdatedAt",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333333"),
+                column: "UpdatedAt",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Permissions");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "DocumentVersions");
+        }
+    }
+}
